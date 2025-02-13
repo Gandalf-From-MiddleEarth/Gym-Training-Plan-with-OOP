7906047c8a1af4375ab1bf772730c373b42e4029
Gandalf the Grey <67333606+Gandalf-From-MiddleEarth@users.noreply.github.com>
2025-02-13 14:41:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,6 @@ public class Main {
             scanner.nextLine();
         }
 
-        System.out.println("Exercise finished czongratulations");
+        System.out.println("Exercise finished congratulations");
     }
 }
